8783b5fb5b89cced2eab36d6afe13921293a0218
Patrick <68784059+PatrickZ3@users.noreply.github.com>
2025-11-21 18:14:13 -0500

    revamp ish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 "use client"
 import Image from "next/image";
-import { motion } from "framer-motion";
-import About from "./components/about";
-// import SkillDetail from "./components/skillDetail"
 import Project from "./components/project"
-import Contact from "./components/contact";
 import Footer from "./components/footer";
-import Experience from "./components/experience";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -78,80 +73,13 @@ export default function Home() {
     };
   }, [isMenuOpen]);
 
-  const toggleMenu = (event: React.MouseEvent) => {
-    event.stopPropagation();
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-  // const programmingLanguages = [
-  //   "Javascript",
-  //   "Python",
-  //   "Typescript",
-  //   "SQL",
-  //   "HTML",
-  //   "CSS",
-  //   "PostgreSQL"
-  // ];
-
-  // const technologies = [
-  //   "Next.js",
-  //   "Node.js",
-  //   "React.js",
-  //   "Tailwind",
-  // ];
-
-  // const developerTools = [
-  //   "Git",
-  //   "Docker",
-  //   "AWS",
-  //   "VSCode"
-  // ];
-
   return (
     <div>
-      <nav className="desktop-nav" id="home">
-        <div className="logo">Patrick<span className="dot">.</span>Lay</div>
-        <div>
-          <ul className="nav-links">
-            {["About", "Skills", "Experiences", "Projects", "Contacts"].map((item) => (
-              <li key={item}>
-                <a href={`#${item.toLowerCase()}`} className="nav-item">
-                  {item}
-                  <motion.div
-                    className="underline"
-                    initial={{ width: 0 }}
-                    animate={{ width: "100%" }}
-                    transition={{ duration: 0.3 }}
-                  />
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </nav>
-      <div className={`overlay ${isMenuOpen ? "show" : ""}`} onClick={() => setIsMenuOpen(false)}></div>
-      <nav className="hamburger-nav">
-        <div className="logo" id="home">Patrick<span className="dot">.</span>Lay</div>
-        <div className="hamburger-menu">
-          <div className="hamburger-icon" onClick={toggleMenu}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-          <div className={`menu-links ${isMenuOpen ? "open" : ""}`}>
-            <li><a href="#about" className="links" onClick={toggleMenu}>About</a></li>
-            <li><a href="#skills" className="links" onClick={toggleMenu}>Skills</a></li>
-            <li><a href="#experiences" className="links" onClick={toggleMenu}>Experiences</a></li>
-            <li><a href="#projects" className="links" onClick={toggleMenu}>Projects</a></li>
-            <li><a href="#contacts" className="links" onClick={toggleMenu}>Contacts</a></li>
-          </div>
-        </div>
-      </nav>
       <section className="profile" id="profile">
         <div className="section__pic-container"></div>
         <div className="section__text">
-          <h1 className="title"> Patrick Lay</h1>
-          <p className="section__text__p2">Software Engineer</p>
+          <h1 className="title"> Patrick<span className="dot">.</span>Lay</h1>
+          <p className="section__text__p2">Full-Stack Software Engineer</p>
           <div className="btn-container">
             <button
               className="btn btn-color-2"
@@ -186,73 +114,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-
-      <section className="about" id="about">
-        <div className="section__text__p1"> Get To Know More</div>
-        <div className="title">About Me</div>
-        <About />
-        <Image
-          src="/arrow.png"
-          alt="arrow"
-          className="icon arrow"
-          width={30}
-          height={30}
-          onClick={() => (window.location.href = "#skills")}
-        />
-      </section>
-      {/* <section className="skills" id="skills">
-        <div className="section__text__p1"> Explore My</div>
-        <div className="title">Skills</div>
-        <div className="skills-details-container">
-          <div className="about-containers">
-            <div className="details-container">
-              <div className="skills-sub-title">Programming Languages</div>
-              <div className="article-container">
-                {programmingLanguages.map((language, index) => (
-                  <SkillDetail key={index} skillName={language} />
-                ))}
-              </div>
-            </div>
-            <div className="details-container">
-              <div className="skills-sub-title">Technologies</div>
-              <div className="article-container">
-                {technologies.map((language, index) => (
-                  <SkillDetail key={index} skillName={language} />
-                ))}
-              </div>
-            </div>
-            <div className="details-container">
-              <div className="skills-sub-title">Developer Tools</div>
-              <div className="article-container">
-                {developerTools.map((language, index) => (
-                  <SkillDetail key={index} skillName={language} />
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-        <Image
-          src="/arrow.png"
-          alt="arrow"
-          className="icon arrow"
-          width={30}
-          height={30}
-          onClick={() => (window.location.href = "#experiences")}
-        />
-      </section> */}
-      <section className="experiences" id="experiences">
-        <div className="section__text__p1">Discover My</div>
-        <div className="title">Experiences</div>
-        <Experience />
-        <Image
-          src="/arrow.png"
-          alt="arrow"
-          className="icon arrow"
-          width={30}
-          height={30}
-          onClick={() => (window.location.href = "#projects")}
-        />
-      </section>
+      
       <section className="projects" id="projects">
         <div className="section__text__p1">My Featured</div>
         <div className="title">Projects</div>
@@ -273,28 +135,6 @@ export default function Home() {
             height={15}
           />
         </button>
-        <Image
-          src="/arrow.png"
-          alt="arrow"
-          className="icon arrow"
-          width={30}
-          height={30}
-          onClick={() => (window.location.href = "#contacts")}
-        />
-      </section>
-      <section className="contacts" id="contacts">
-        <div className="section__text__p1">Get in Touch</div>
-        <div className="title">Contact Me</div>
-        <Contact />
-        <Image
-          src="/arrow.png"
-          alt="arrow"
-          className="icon arrow"
-          width={30}
-          height={30}
-          style={{ rotate: "180deg" }}
-          onClick={() => (window.location.href = "#home")}
-        />
       </section>
 
       <Footer />
